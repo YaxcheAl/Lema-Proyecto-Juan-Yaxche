2d542ca653cad00bfeedbbbf69b1c50bda9f1292
juanmata25 <juanandresmataz40@gmail.com>
2025-05-05 09:57:48 +0200

    tabla creada, inicio de insert into

--- vitae.sql
+++ vitae.sql
@@ -2,7 +2,12 @@ create table curriculums (
     id NUMBER PRIMARY KEY,
     nombre VARCHAR(100),
     telefono VARCHAR(100),
-    email VARCHAR(100),
     fecha_creacion DATE,
     datos_cv XMLTYPE
+);
+
+INSERT INTO curriculums VALUES (
+    1, 'Juan Perez', 621348745, 
+    XMLTYPE('
+    ')
 );
