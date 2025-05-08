b0ca419d1e9d1bf7d6bcfdebb5597df790a11c00
juanmata25 <juanandresmataz40@gmail.com>
2025-05-08 14:19:25 +0200

    updates y constulas de campos no xml: Hechas falta comprobar las 2 ultimas a ver si se ejecutan correctamente falta: consultas con salida xml XMLELEMENT XMLATRIBUTTES, ETC

---
+++ consultas_curriculum.sql
@@ -0,0 +1,24 @@
+--Modificar el email de la persona cuyo nombre es Pedro Mata 
+update curriculums
+set datos_cv = updatexml(datos_cv, 'curriculum/personal/email/text()','pedromataperez@gmail.com')
+where nombre = 'Pedro Mata'; 
+
+--Modificar el numero de telefono del dueño de este correo electronico: angelobando@gmail.com
+update curriculums
+set telefono = 621454556
+where extractvalue(datos_cv, 'curriculum/personal/email/text()') = 'angelobando@gmail.com';
+
+--Modificar el numero de telefono de Jhonny Marin
+update curriculums
+set telefono = 621464156
+where nombre = 'Jhonny Marin';
+
+--Sacar por pantalla el titulo de las personas que han trabajado en Microsoft
+select extractvalue(DATOS_CV, 'curriculum/educacion/titulo/text()')
+from curriculums
+where extractvalue(DATOS_CV, 'curriculum/experiencia/trabajo') = 'Microsoft';
+
+--Sacar por pantalla el nombre de las personas que tienen un titulo llamado "Máster en Marketing Digital"
+select extractvalue(DATOS_CV, 'curriculum/empresa/text()')
+from curriculums
+where extractvalue(DATOS_CV, 'curriculum/educacion/titulo') = 'Máster en Marketing Digital';
