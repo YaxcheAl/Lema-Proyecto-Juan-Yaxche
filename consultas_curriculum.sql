f744ae97ae2d378dbbb90478252c6b666be84488
juanmata25 <juanandresmataz40@gmail.com>
2025-05-12 09:00:50 +0200

    Consultas del punto 1 y 2 funcionan correctamente

--- consultas_curriculum.sql
+++ consultas_curriculum.sql
@@ -13,12 +13,12 @@ update curriculums
 set telefono = 621464156
 where nombre = 'Jhonny Marin';
 
---Sacar por pantalla el titulo de las personas que han trabajado en Microsoft
-select extractvalue(DATOS_CV, 'curriculum/educacion/titulo')
+--Sacar por pantalla la institucion donde se formaron las personas que han trabajado en Microsoft
+select extractvalue(DATOS_CV, 'curriculum/educacion/institucion')
 from curriculums
-where extractvalue(DATOS_CV, 'curriculum/experiencia/trabajo') = 'Microsoft';
+WHERE existsNode(DATOS_CV, '/curriculum/experiencia/trabajo[empresa="Microsoft"]') = 1;
 
---Sacar por pantalla el nombre de las personas que tienen un titulo llamado "Máster en Marketing Digital"
-select extractvalue(DATOS_CV, 'curriculum/empresa')
+--Sacar por pantalla el nombre de las personas que tienen un titulo llamado "Grado en Ciencias de la Computacion"
+select nombre
 from curriculums
-where extractvalue(DATOS_CV, 'curriculum/educacion/titulo') = 'Máster en Marketing Digital';
+where extractvalue(DATOS_CV, 'curriculum/educacion/titulo') = 'Grado en Ciencias de la Computacion';
